d8bb68b4a9cbcfa59dc222c6f1216dc4edca5e5a
OppNHeimer <maxoppenheimer1993@gmail.com>
2020-09-14 18:30:13 -0500

    move files to test

---
+++ test/hashketball.rb
@@ -0,0 +1,162 @@
+# Write your code below game_hash
+require "pry"
+
+def game_hash
+  {
+    home: {
+      team_name: "Brooklyn Nets",
+      colors: ["Black", "White"],
+      players: [
+        {
+          player_name: "Alan Anderson",
+          number: 0,
+          shoe: 16,
+          points: 22,
+          rebounds: 12,
+          assists: 12,
+          steals: 3,
+          blocks: 1,
+          slam_dunks: 1
+        },
+        {
+          player_name: "Reggie Evans",
+          number: 30,
+          shoe: 14,
+          points: 12,
+          rebounds: 12,
+          assists: 12,
+          steals: 12,
+          blocks: 12,
+          slam_dunks: 7
+        },
+        {
+          player_name: "Brook Lopez",
+          number: 11,
+          shoe: 17,
+          points: 17,
+          rebounds: 19,
+          assists: 10,
+          steals: 3,
+          blocks: 1,
+          slam_dunks: 15
+        },
+        {
+          player_name: "Mason Plumlee",
+          number: 1,
+          shoe: 19,
+          points: 26,
+          rebounds: 11,
+          assists: 6,
+          steals: 3,
+          blocks: 8,
+          slam_dunks: 5
+        },
+        {
+          player_name: "Jason Terry",
+          number: 31,
+          shoe: 15,
+          points: 19,
+          rebounds: 2,
+          assists: 2,
+          steals: 4,
+          blocks: 11,
+          slam_dunks: 1
+        }
+      ]
+    },
+    away: {
+      team_name: "Charlotte Hornets",
+      colors: ["Turquoise", "Purple"],
+      players: [
+        {
+          player_name: "Jeff Adrien",
+          number: 4,
+          shoe: 18,
+          points: 10,
+          rebounds: 1,
+          assists: 1,
+          steals: 2,
+          blocks: 7,
+          slam_dunks: 2
+        },
+        {
+          player_name: "Bismack Biyombo",
+          number: 0,
+          shoe: 16,
+          points: 12,
+          rebounds: 4,
+          assists: 7,
+          steals: 22,
+          blocks: 15,
+          slam_dunks: 10
+        },
+        {
+          player_name: "DeSagna Diop",
+          number: 2,
+          shoe: 14,
+          points: 24,
+          rebounds: 12,
+          assists: 12,
+          steals: 4,
+          blocks: 5,
+          slam_dunks: 5
+        },
+        {
+          player_name: "Ben Gordon",
+          number: 8,
+          shoe: 15,
+          points: 33,
+          rebounds: 3,
+          assists: 2,
+          steals: 1,
+          blocks: 1,
+          slam_dunks: 0
+        },
+        {
+          player_name: "Kemba Walker",
+          number: 33,
+          shoe: 15,
+          points: 6,
+          rebounds: 12,
+          assists: 12,
+          steals: 7,
+          blocks: 5,
+          slam_dunks: 12
+        }
+      ]
+    }
+  }
+end
+
+def num_points_scored(player_name)
+  player_points_scored = nil
+
+  game_hash.values.each do |team_info|
+    players = team_info[:players]
+
+    players.each do |player|
+      if player[:player_name] == player_name 
+        player_points_scored = player[:points]
+      end
+    end
+  end
+
+  player_points_scored
+end
+
+def big_shoe_rebounds
+  players_arrays = game_hash.values.map do |player_info|
+    player_info[:players]
+  end
+
+  flattened_players_array = players_arrays.flatten
+  
+
+  return flattened_players_array
+  # get a list of all players
+  # get the player with the largest shoes size
+  # return that players number of rebounds
+end
+
+num_points_scored("Alan Anderson")
+big_shoe_rebounds
---
+++ test/lesson.md
@@ -0,0 +1,37 @@
+# Intro to Object Oriented Programming in Ruby
+
+## Objectives
+
+- data types in Ruby
+    - distinguish between different data types
+    - demonstrate use of common `Array` methods
+- define "single responsibility"
+- debugging & problem solving
+    - demonstrate good googling and documentation use
+    - demonstrate TDD (test-driven development) best practices
+    - demonstrate use of pry
+
+## Outline
+```
+2m  Intro to Lectures
+3m  Working with the spec and intro TDD
+3m  Build the game hash
+10m Define the problem
+5m  Digression on `map`
+10m Student Exercise
+5m  Apply to `get_players`
+5m  Use `binding.pry` to demonstrate how to debug `get_players`
+10m Digression with `select` and `find`
+10m Single Responsibility Priniciple (SRP) and putting it together
+===
+1h  Total 
+```
+
+## Questions
+
+
+## Icebox
+
+## Resources
+
+https://ruby-doc.org/ - great general ruby docs
