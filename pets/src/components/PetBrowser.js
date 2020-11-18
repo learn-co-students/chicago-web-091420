5f5380731ba185e2cf258af8b838ea22d4128f80
Raza Jafri <sjafri@SF-US04304-MBP13.local>
2020-11-18 16:37:56 -0600

    asdf

---
+++ pets/src/components/PetBrowser.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+import Pet from './Pet'
+
+class PetBrowser extends React.Component {
+
+  render() {
+    return <div className="ui cards">
+      {
+        this.props.pets.map(petObj => {
+          return <Pet key={petObj.id} {...petObj} onAdoptPet={this.props.onAdoptPet} />
+        })
+      }
+
+    </div>
+  }
+}
+
+export default PetBrowser
